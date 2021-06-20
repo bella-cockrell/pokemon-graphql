6ae8c95371b2c95e37bfc90fd19c015071f07c8c
bella-cockrell <heybellac@gmail.com>
2021-06-20 21:38:29 +0100

    Import dependencies

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 function App() {
   return (
     <div className="App">
